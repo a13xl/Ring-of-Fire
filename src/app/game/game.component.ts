7aaa04cabd944e21d04e4f5cc705960eae68e55c
a13xl <106622336+a13xl@users.noreply.github.com>
2023-04-11 21:30:27 +0200

    added playing Cards and starting with adding Player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
   pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if(!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.currentCard);
+      }, 1000);
+    }
   }
 }
