1d5907900cf1ca3a244af8103d35761a70e4a035
a13xl <106622336+a13xl@users.noreply.github.com>
2023-04-12 21:08:21 +0200

    added dialog add player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 @Component({
   selector: 'app-game',
@@ -10,6 +13,8 @@ export class GameComponent implements OnInit{
   pickCardAnimation = false;
   currentCard: string = '';
   game: Game;
+
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit() {
     this.newGame();
@@ -29,5 +34,15 @@ export class GameComponent implements OnInit{
         this.game.playedCards.push(this.currentCard);
       }, 1000);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if(name) {
+        this.game.players.push(name);
+      }
+    });
   }
 }
